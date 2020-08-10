86526b56ad04a1083256b67cf7c1c6ad16288411
Dragorn421 <m.arthur@orange.fr>
2020-08-10 12:59:00 +0200

    Made updater pull from 2.8x branch

--- io_export_objex2/addon_updater_ops.py
+++ io_export_objex2/addon_updater_ops.py
@@ -1454,7 +1454,7 @@ def register(bl_info):
 	# updater.include_branch_list defaults to ['master'] branch if set to none
 	# example targeting another multiple branches allowed to pull from
 	# updater.include_branch_list = ['master', 'dev'] # example with two branches
-	updater.include_branch_list = None  # None is the equivalent to setting ['master']
+	updater.include_branch_list = ['2.8x']
 
 	# Only allow manual install, thus prompting the user to open
 	# the addon's web page to download, specifically: updater.website
